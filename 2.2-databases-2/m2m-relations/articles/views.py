b0aa4cd2a2df72de56ab69a51f285705fb49b930
Mladinsky <cevtrem@gmail.com>
2025-04-28 18:18:29 +0300

    Finish task 2.2-databases-2

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
+from django.db.models import Prefetch
 
-from articles.models import Article
+from articles.models import Article, Scope
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    articles = Article.objects.prefetch_related(
+        Prefetch(
+            'scopes',
+            queryset=Scope.objects.select_related('tag').order_by('-is_main', 'tag__name')
+        )
+    ).order_by('-published_at')
+
+    context = {
+        'object_list': articles
+    }
 
     return render(request, template, context)
